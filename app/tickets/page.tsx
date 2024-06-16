6dcfd57bb653e1b8d57e0361a74a0c0ca8490ddd
Ashcode <akshaycodeur@gmail.com>
2024-06-16 17:29:01 -0400

    chore: locale date string update

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -2,9 +2,8 @@ import prisma from '@/prisma/db';
 
 const Tickets = async () => {
   const tickets = await prisma.ticket.findMany();
-  console.log(tickets);
   return (
-    <div>
+    <>
       <table class="table-auto">
         <thead>
           <tr className="p-2 bg-slate-200 space-x-4">
@@ -18,20 +17,22 @@ const Tickets = async () => {
           </tr>
         </thead>
         <tbody>
-          {tickets.map((ticket) => (
-            <tr className="border-b">
-              <td>{ticket.id}</td>
-              <td>{ticket.title}</td>
-              <td>{ticket.description}</td>
-              <td>{ticket.status}</td>
-              <td>{ticket.priority}</td>
-              <td></td>
-              <td></td>
-            </tr>
-          ))}
+          {tickets
+            ? tickets.map((ticket) => (
+                <tr className="border-b" key={ticket.id}>
+                  <td>{ticket.id}</td>
+                  <td>{ticket.title}</td>
+                  <td>{ticket.description}</td>
+                  <td>{ticket.status}</td>
+                  <td>{ticket.priority}</td>
+                  <td>{ticket.createdAt.toLocaleDateString()}</td>
+                  <td>{ticket.updatedAt.toLocaleDateString()}</td>
+                </tr>
+              ))
+            : null}
         </tbody>
       </table>
-    </div>
+    </>
   );
 };
 
